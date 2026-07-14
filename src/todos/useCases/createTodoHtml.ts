a80a30ecc25b6b21980bbf1fdb4e1d3aa7e9d52b
seiku2000 <eslumter@gmail.com>
2026-07-13 17:55:01 -0700

    renderyTodo

--- src/todos/useCases/createTodoHtml.ts
+++ src/todos/useCases/createTodoHtml.ts
@@ -2,10 +2,37 @@ import type { Todo } from "../models";
 import type { CreateTodoHtmlInterface } from "./interfaces/renderTodoInter";
 
 export const createTodoHtml:CreateTodoHtmlInterface = (todo:Todo) => {
-    const html = `<h1>${todo.description}</h1>`;
+    
+    //<li class="completed" data-id="abc">
 
-    const liElement =document.createElement('li');
+    const { id,done,description} = todo
+    const html = `
+    
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${done ? 'checked' : ''}>
+                    <label>${description}</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template">
+            </li> 
+            <!-- <li>
+                <div class="view">
+                    <input class="toggle" type="checkbox">
+                    <label>Comprar un unicornio</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Rule the web">
+            
+    
+    `;
+
+    const liElement =document.createElement('li');  
      liElement.innerHTML = html;
+     liElement.setAttribute('data-id',id);
+     if(done){
+        liElement.classList.add('completed');
+     }
+     
 
 
     return liElement;
--- src/todos/useCases/renderTodos.ts
+++ src/todos/useCases/renderTodos.ts
@@ -2,11 +2,22 @@
 import {  Todo} from "../models/";
 import { createTodoHtml } from "./createTodoHtml";
 import type { RenderTodoInterface } from "./interfaces/renderTodoInter";
+ 
+let element:HTMLElement 
+
 
 
 export const renderTodos:RenderTodoInterface =(elementId:string, todos:Todo[]) =>{
  console.log(elementId, todos);
- const element =document.querySelector(elementId);
+ //const element =document.querySelector(elementId);
+ if(!element) 
+   element = document.querySelector(elementId);
+
+ if(!element) 
+   throw new Error(`Element ${elementId} not found`);
+
+ element.innerHTML = '';
+ 
  //console.log(element);
  todos.forEach(todo => {
     element?.append(createTodoHtml(todo))
